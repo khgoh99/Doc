be9523a2215cf6758f851f52ccf16632cf1ceabc
khgoh99 <51729388+khgoh99@users.noreply.github.com>
2019-07-07 17:21:41 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -10,5 +10,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
 sudo usermod -aG docker "${USER}"
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
